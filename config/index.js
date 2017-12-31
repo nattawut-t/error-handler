38dab8377949a65a323d2ac5105072fe920a3388
Nattawut T <nattawut.t@kbtg.tech>
2017-12-31 20:31:19 +0700

    initial commit

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  request_timeout: {
+    code: 408,
+    message: 'request timeout',
+  },
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+const handleMessage = require('./lib/handleMessage')
+
+module.exports = {
+  handleMessage,
+}
